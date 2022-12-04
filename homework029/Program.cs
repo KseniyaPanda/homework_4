41b5cdf60bcd9a9a054600b168424592aaf2d035
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-04 20:04:51 +0300

    решение 29 задачи

--- homework029/Program.cs
+++ homework029/Program.cs
@@ -4,3 +4,27 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+
+int[] InitArray(int length)
+{
+   int[] array = new int[length];
+   Random rnd = new Random();
+   for (int i = 0; i < length; i++)
+   {
+      array[i] = rnd.Next(0, 10);
+   }
+   return array;
+}
+
+void PrintArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+
+int[] array = InitArray(8);
+PrintArray(array);
