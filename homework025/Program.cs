84dad9a16a281b033f6de4dea09828934c2c7fb3
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-04 21:33:24 +0300

    решение 25 задачи

--- homework025/Program.cs
+++ homework025/Program.cs
@@ -4,3 +4,35 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+
+int GetNumber(string message)
+{
+   int result = 0;
+   bool isCorrect = false;
+
+   while (!isCorrect)
+   {
+      Console.WriteLine(message);
+
+      if (int.TryParse(Console.ReadLine(), out result))
+      {
+         isCorrect = true;
+      }
+      else
+      {
+         Console.WriteLine("Введите корректное число");
+      }
+   }
+
+   return result;
+}
+
+int number = GetNumber("введите число");
+int extent = GetNumber("введите степень");
+int result = 1;
+for (int i = 1; i <= extent; i++)
+{
+   result = result * number;
+}
+
+Console.WriteLine($"число {number} в степени {extent} = {result}");
