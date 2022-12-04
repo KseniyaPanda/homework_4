35167cc87ea6eb250359831befcf2a72684b7a2f
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-04 21:18:21 +0300

    решение 27 задачи

--- homework027/Program.cs
+++ homework027/Program.cs
@@ -5,3 +5,37 @@
 82 -> 10
 9012 -> 12
 */
+
+int GetNumber(string message)
+{
+   int result = 0;
+   bool isCorrect = false;
+
+   while (!isCorrect)
+   {
+      Console.WriteLine(message);
+
+      if (int.TryParse(Console.ReadLine(), out result))
+      {
+         isCorrect = true;
+      }
+      else
+      {
+         Console.WriteLine("Ввели не число.");
+      }
+   }
+
+   return result;
+}
+
+int number = GetNumber("Введите число");
+
+int sum = 0;
+
+for (int i = number; i > 0;)
+{
+   sum = sum + i % 10;
+   i = i / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {number} = {sum}");
